40e2cea41b6b078745dfb68982e3b3925a8b3af5
Petr Or <petronaooliver@gmail.com>
2026-06-23 23:07:30 -0300

    add list god of war1

--- dados.js
+++ dados.js
@@ -33,3 +33,65 @@ export const TODOS_OS_TEMAS = [
   { id: '3', titulo: "🔗 Conectivos e Transições", dados: CONECTIVOS },
   { id: '4', titulo: "🧠 Teste da Palavra Bound", dados: TESTE_BOUND },
 ];
+
+const palavrasGodOfWar = [
+  // ⚔️ Armas, Combate e Poderes
+  { termo: "Blade", significado: "Lâmina", pronuncia: "Blêid" },
+  { termo: "Chain", significado: "Corrente", pronuncia: "Tchêin" },
+  { termo: "Sword", significado: "Espada", pronuncia: "Sórd" },
+  { termo: "Shield", significado: "Escudo", pronuncia: "Chíld" },
+  { termo: "Spear", significado: "Lança", pronuncia: "Spír" },
+  { termo: "Rage", significado: "Fúria / Ira", pronuncia: "Rêidj" },
+  { termo: "Magic", significado: "Magia", pronuncia: "Médiic" },
+  { termo: "Lightning", significado: "Raio / Relâmpago", pronuncia: "Láit-ning" },
+  { termo: "Gaze", significado: "Olhar / Olhar fixo", pronuncia: "Gêiz" },
+  { termo: "Soul", significado: "Alma", pronuncia: "Sôul" },
+  { termo: "Wrath", significado: "Ira divina / Rancor", pronuncia: "Uróf" },
+  { termo: "Strike", significado: "Golpe / Atacar", pronuncia: "Stráik" },
+
+  // 🏛️ História, Deuses e Mitologia
+  { termo: "God", significado: "Deus", pronuncia: "Gód" },
+  { termo: "War", significado: "Guerra", pronuncia: "Uór" },
+  { termo: "Ghost", significado: "Fantasma", pronuncia: "Gôust" },
+  { termo: "Ash", significado: "Cinza", pronuncia: "Éch" },
+  { termo: "Oracle", significado: "Oráculo", pronuncia: "Óra-col" },
+  { termo: "Temple", significado: "Templo", pronuncia: "Têm-pol" },
+  { termo: "Desert", significado: "Deserto", pronuncia: "Dé-zert" },
+  { termo: "Pandora's Box", significado: "Caixa de Pandora", pronuncia: "Pan-dó-raz Bóks" },
+  { termo: "Titan", significado: "Titã", pronuncia: "Tái-tan" },
+  { termo: "Mortal", significado: "Mortal", pronuncia: "Mór-tal" },
+  { termo: "Olympo", significado: "Olimpo", pronuncia: "O-lím-po" },
+  { termo: "Underworld", significado: "Submundo / Inferno", pronuncia: "Ânder-uórld" },
+  { termo: "Throne", significado: "Trono", pronuncia: "Trôun" },
+
+  // 👹 Inimigos e Criaturas
+  { termo: "Monster", significado: "Monstro", pronuncia: "Móns-ter" },
+  { termo: "Beast", significado: "Fera / Besta", pronuncia: "Bíst" },
+  { termo: "Minotaur", significado: "Minotauro", pronuncia: "Mí-no-tór" },
+  { termo: "Cyclops", significado: "Ciclope", pronuncia: "Sái-clops" },
+  { termo: "Siren", significado: "Sereia", pronuncia: "Sái-ren" },
+  { termo: "Harpy", significado: "Hárpia", pronuncia: "Hár-pi" },
+  { termo: "Hydra", significado: "Hidra", pronuncia: "Hái-dra" },
+  { termo: "Soldier", significado: "Soldado", pronuncia: "Sôul-djer" },
+
+  // 🧭 Exploração, Quebra-cabeças e Itens
+  { termo: "Chest", significado: "Baú", pronuncia: "Tchést" },
+  { termo: "Feather", significado: "Pena", pronuncia: "Fé-der" },
+  { termo: "Eye", significado: "Olho", pronuncia: "Ái" },
+  { termo: "Key", significado: "Chave", pronuncia: "Quí" },
+  { termo: "Gate", significado: "Portão", pronuncia: "Guêit" },
+  { termo: "Lever", significado: "Alavanca", pronuncia: "Lé-ver" },
+  { termo: "Crank", significado: "Manivela", pronuncia: "Crénk" },
+  { termo: "Bridge", significado: "Ponte", pronuncia: "Brídj" },
+  { termo: "Cliff", significado: "Despenhadeiro", pronuncia: "Clíf" },
+  { termo: "Trap", significado: "Armadilha", pronuncia: "Trép" },
+
+  // 🩸 Sentimentos, Destino e Vingança
+  { termo: "Vengeance", significado: "Vingança", pronuncia: "Vên-djans" },
+  { termo: "Revenge", significado: "Revanche / Vingança", pronuncia: "Ri-vêndj" },
+  { termo: "Nightmare", significado: "Pesadelo", pronuncia: "Náit-mêir" },
+  { termo: "Sins", significado: "Pecados", pronuncia: "Síns" },
+  { termo: "Fate", significado: "Destino", pronuncia: "Fêit" },
+  { termo: "Curse", significado: "Maldição", pronuncia: "Cârs" },
+  { termo: "Betrayal", significado: "Traição", pronuncia: "Bi-trêi-al" }
+];
